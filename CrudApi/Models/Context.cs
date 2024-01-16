0d82b66904f2eddd0f63a2d244384b992bba414d
Alexandro Jorge <alexandro.jorge@proway.treina>
2024-01-16 17:09:11 -0300

    api finalizada

---
+++ CrudApi/Models/Context.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CrudApi.Models
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Categorias> Categoria { get; set; } = null!;
+        public DbSet<Produto> Produto { get; set; } = null!;
+
+    }
+}
---
+++ CrudApi/Produto.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CrudApi
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public string Preco { get; set; }
+        public int Estoque { get; set; }
+        public Categorias Categoria { get; set; }
+
+        [JsonIgnore]
+        public int CategoriaId { get; set; }
+
+    }
+}
